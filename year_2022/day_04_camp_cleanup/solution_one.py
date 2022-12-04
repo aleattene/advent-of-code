77959791e5127e0678eae60cd04bbf84ea44ab0a
aleattene <attene.tomorrowdevs@gmail.com>
2022-12-04 15:57:37 +0100

    fix(year-2022/day-04): fixed syntax error

--- year_2022/day_04_camp_cleanup/solution_one.py
+++ year_2022/day_04_camp_cleanup/solution_one.py
@@ -15,7 +15,7 @@ def get_overlapping_sections(data):
     :param data: list
     :return: int
     """
-    return sum([1 for pos in data if pos[0] >= pos[2] and pos[1] <= pos[3] or pos[2] >= pos[0] and pos[3] <= pos[1]])
+    return sum([1 for pos in data if (pos[0] >= pos[2] and pos[1] <= pos[3]) or (pos[2] >= pos[0] and pos[3] <= pos[1])])
 
 
 if __name__ == '__main__':
