a9de52e6f1045691509f9c94c7c33889082339f8
aleattene <attene.tomorrowdevs@gmail.com>
2024-10-15 14:39:27 +0200

    chore(01/2015): improve solution two day 01/2015

--- year_2015/day_01_not_quite_lisp/solution_two_01_2015.py
+++ year_2015/day_01_not_quite_lisp/solution_two_01_2015.py
@@ -1,18 +1,17 @@
 
 def find_index_position_enter_basement(filename):
+    floor = 0
     elevator_map = {
         "(": 1,
         ")": -1
     }
-    floor = 0
-    position = 0
 
     with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            for symbol in line:
-                position += 1
-                if symbol in elevator_map:
-                    floor += elevator_map[symbol]
-                if floor < 0:
-                    return position
+        data = f.read()
+
+    for position, symbol in enumerate(data, start=1):
+        if symbol in elevator_map:
+            floor += elevator_map[symbol]
+        if floor < 0:
+            return position
 
