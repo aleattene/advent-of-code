315c9a99e2ccaf3f527dba391fbbebeec860b7ad
aleattene <attene.tomorrowdevs@gmail.com>
2024-11-19 20:13:53 +0100

    refactor(01/2015): add demo-input, test and improve readme description

--- year_2015/day_01_not_quite_lisp/test_day_01_2015.py
+++ year_2015/day_01_not_quite_lisp/test_day_01_2015.py
@@ -2,18 +2,25 @@ import os
 from .solution_one_01_2015 import find_floor
 from .solution_two_01_2015 import find_index_position_enter_basement
 
+filename_demo = "input_demo.txt"
 filename = "input.txt"
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path_demo = os.path.join(current_dir, filename_demo)
 file_path = os.path.join(current_dir, filename)
 
 
 def test_first_part():
-    result = find_floor(file_path)
     # TO FIX -> Handler error opening file
+    result_demo = find_floor(file_path_demo)
+    assert result_demo == -3
+    result = find_floor(file_path)
     assert result == 138
 
 
 def test_second_part():
+    result_demo = find_index_position_enter_basement(file_path_demo)
+    assert result_demo == 43
     result = find_index_position_enter_basement(file_path)
     assert result == 1771
 
