2a48aabefcbf4fde565c41d3435d41a92414b577
aleattene <attene.tomorrowdevs@gmail.com>
2024-12-07 12:08:34 +0100

    refactor(01/2015): add env variables to diff local and prod tests

--- year_2015/day_01_not_quite_lisp/README.md
+++ year_2015/day_01_not_quite_lisp/README.md
@@ -2,8 +2,3 @@
 
 https://adventofcode.com/2015/day/1
 
-Part One: `138`
-
-Part Two: `1771`
-
-*Demo: `-3` and `43`*
--- year_2015/day_01_not_quite_lisp/test_day_01_2015.py
+++ year_2015/day_01_not_quite_lisp/test_day_01_2015.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
 from .solution_one_01_2015 import find_floor
 from .solution_two_01_2015 import find_index_position_enter_basement
+
+load_dotenv()
+environment = os.getenv("ENVIRONMENT")
 
 filename_demo = "input_demo.txt"
 filename = "input.txt"
@@ -9,18 +13,15 @@ file_path_demo = os.path.join(current_dir, filename_demo)
 file_path = os.path.join(current_dir, filename)
 
 
-def test_first_part():
-    # TO FIX -> Handler error opening file
+def test_day_01_2015():
     result_demo = find_floor(file_path_demo)
     assert result_demo == -3
-    result = find_floor(file_path)
-    assert result == 138
-
-
-def test_second_part():
-    # TO FIX -> Handler error opening file
     result_demo = find_index_position_enter_basement(file_path_demo)
     assert result_demo == 43
-    result = find_index_position_enter_basement(file_path)
-    assert result == 1771
-
+    if environment == "development":
+        expected_result_one = int(os.getenv("SOLUTION_01_DAY_01_2015"))
+        expected_result_two = int(os.getenv("SOLUTION_02_DAY_01_2015"))
+        result_one = find_floor(file_path)
+        result_two = find_index_position_enter_basement(file_path)
+        assert expected_result_one == result_one
+        assert expected_result_two == result_two
