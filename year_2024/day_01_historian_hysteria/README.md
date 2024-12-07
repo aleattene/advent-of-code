6c13bcd915c8cccc4484c0fde903a35a9145dc8c
aleattene <attene.tomorrowdevs@gmail.com>
2024-12-07 17:09:12 +0100

    refactor(01/2024): add env variables to diff local and prod tests

--- year_2024/day_01_historian_hysteria/README.md
+++ year_2024/day_01_historian_hysteria/README.md
@@ -2,8 +2,3 @@
 
 https://adventofcode.com/2024/day/1
 
-Part One: `936063`
-
-Part Two: `23150395`
-
-*Demo: `11` and `31`*
--- year_2024/day_01_historian_hysteria/test_day_01_2024.py
+++ year_2024/day_01_historian_hysteria/test_day_01_2024.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
 from .solution_day_01_2024 import solve_day_01_2024
+
+load_dotenv
+environment = os.getenv("ENVIRONMENT")
 
 filename_demo = "input_demo.txt"
 filename = "input.txt"
@@ -9,7 +13,10 @@ file_path = os.path.join(current_dir, filename)
 
 
 def test_day_01_2024():
-    result_demo = solve_day_01_2024(file_path_demo)
-    assert result_demo == (11, 31)
-    result_one = solve_day_01_2024(file_path)
-    assert result_one == (936063, 23150395)
+    results_demo = solve_day_01_2024(file_path_demo)
+    assert results_demo == (11, 31)
+    if environment == "development":
+        expected_results = (int(os.getenv("SOLUTION_01_DAY_01_2024")),
+                            int(os.getenv("SOLUTION_02_DAY_01_2024")))
+        results = solve_day_01_2024(file_path)
+        assert expected_results == results
