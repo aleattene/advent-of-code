07a2f3fa335097a49f64b39b512030ee163b67db
aleattene <attene.tomorrowdevs@gmail.com>
2024-12-07 12:15:54 +0100

    refactor(02/2015): add env variables to diff local and prod tests

--- year_2015/day_02_i_was_told_no_math/README.md
+++ year_2015/day_02_i_was_told_no_math/README.md
@@ -1,9 +1,3 @@
 ## Day 02/2015: I Was Told There Would Be No Math
 
 https://adventofcode.com/2015/day/2
-
-Part One: `1598415`
-
-Part Two: `3812909`
-
-*Demo: `101` and `48`*
--- year_2015/day_02_i_was_told_no_math/test_day_02_2015.py
+++ year_2015/day_02_i_was_told_no_math/test_day_02_2015.py
@@ -1,6 +1,10 @@
 import os
+from dotenv import load_dotenv
 from .solution_one_day_02_2015 import solve_day_02_2015_one
 from .solution_two_day_02_2015 import solve_day_02_2015_two
+
+load_dotenv()
+environment = os.getenv("ENVIRONMENT")
 
 filename_demo = "input_demo.txt"
 filename = "input.txt"
@@ -9,17 +13,15 @@ file_path_demo = os.path.join(current_dir, filename_demo)
 file_path = os.path.join(current_dir, filename)
 
 
-def test_day_02_2015_one():
-    # TO FIX -> Handler error opening file
+def test_day_02_2015():
     result_demo = solve_day_02_2015_one(file_path_demo)
     assert result_demo == 101
-    result = solve_day_02_2015_one(file_path)
-    assert result == 1598415
-
-
-def test_day_02_2015_two():
-    # TO FIX -> Handler error opening file
     result_demo = solve_day_02_2015_two(file_path_demo)
     assert result_demo == 48
-    result = solve_day_02_2015_two(file_path)
-    assert result == 3812909
+    if environment == "development":
+        expected_result_one = int(os.getenv("SOLUTION_01_DAY_02_2015"))
+        expected_result_two = int(os.getenv("SOLUTION_02_DAY_02_2015"))
+        result_one = solve_day_02_2015_one(file_path)
+        result_two = solve_day_02_2015_two(file_path)
+        assert expected_result_one == result_one
+        assert expected_result_two == result_two
