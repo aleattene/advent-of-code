598a23cf1941fbf2c63b5424b91b2ba64040516c
aleattene <aleattene@gmail.com>
2025-05-26 12:17:08 +0200

    chore(2024): improve solutions and tests day 01/2024

--- year_2024/day_01_historian_hysteria/solution_day_01_2024.py
+++ year_2024/day_01_historian_hysteria/solution_day_01_2024.py
@@ -2,35 +2,63 @@ from collections import Counter
 from utils.file_utils import get_input_file_path, read_input_file
 
 
-def solve_day_01_2024(filename: str) -> tuple[int, int] | str:
-
+def solve_day_01_2024(filename: str) -> tuple[int, int]:
+    """
+    Solution of the Advent of Code 2024 Day 01 - Historian Hysteria.
+    :param filename: The name file containing the input data.
+    :return:
+    """
     try:
         # Create the absolute path of the input file
         input_file_path = get_input_file_path(__file__, filename)
-        # Read the input file
-        data = read_input_file(input_file_path)
+        with open(input_file_path, 'r', encoding='utf-8') as f:
+            # Read the input file
+            data = f.read().split("\n")
     except Exception as error:
-        return f"Error: {error}"
+        raise RuntimeError(f"Error: {error}")
 
-    # Parse the data into left and right lists
-    left_list = []
-    right_list = []
+    # Parse the data into two sorted lists of integers
+    left_list: list[int] = []
+    right_list: list[int] = []
     for line in data:
         left, right = map(int, line.split())
         left_list.append(left)
         right_list.append(right)
-
-    # Sort the left and right lists
     left_list.sort()
     right_list.sort()
 
-    # Part One - Calculate the sum of the absolute differences between the left and right lists
-    sum_differences = sum(abs(left - right) for left, right in zip(left_list, right_list))
+    # Calculate the sum of the absolute differences between the lists (part 1)
+    sum_differences: int = sum(abs(left - right) for left, right in zip(left_list, right_list))
 
     # Create a Counter object for the right list
-    right_counter = Counter(right_list)
+    right_counter: Counter[int] = Counter(right_list)
 
-    # Part Two - Calculate the similarity score between the left and right lists
-    similarity_score = sum(value * right_counter[value] for value in left_list)
+    # Calculate the similarity score between the lists (part 2)
+    similarity_score: int = sum(value * right_counter[value] for value in left_list)
 
     return sum_differences, similarity_score
+
+
+
+if __name__ == "__main__":
+    # Import function to print results
+    import time
+    from utils.file_utils import print_day_results
+
+    # Calculate results for demo and real input files
+    demo_1, demo_2 = solve_day_01_2024("input_demo.txt")
+    start_time = time.time()
+    solution_1, solution_2 = solve_day_01_2024("input.txt")
+    end_time = time.time()
+
+    # Calculate execution time in milliseconds
+    execution_time: int = int((end_time - start_time) * 1000)
+
+    # Print results in a formatted table (using rich)
+    print_day_results(
+        "2024", "01",
+        str(demo_1), str(demo_2),
+        str(solution_1), str(solution_2),
+        execution_time
+    )
+
